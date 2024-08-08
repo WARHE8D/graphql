d5f7f099d36a99fb6c2df85fafd427d8bea81687
WARHE8D <murtuzasalman18@gmail.com>
2024-08-08 10:44:47 -0400

    added test case

--- src/main/java/com/warhe8d/controller/Controller.java
+++ src/main/java/com/warhe8d/controller/Controller.java
@@ -5,13 +5,12 @@ import com.warhe8d.records.Book;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
-public class BookController {
+@org.springframework.stereotype.Controller
+public class Controller {
     @QueryMapping
     public List<Book> books(){
         return Book.books;
---
+++ src/test/java/com/warhe8d/controller/ControllerTest.java
@@ -0,0 +1,12 @@
+package com.warhe8d.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void books() {
+    }
+}
